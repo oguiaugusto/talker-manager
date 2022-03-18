ac54c861c3ecba4415119f4f2ece0774c879c7e4
Guilherme <guilhaugusto05@gmail.com>
2022-03-18 00:04:33 -0300

    Create endpoint GET /talker/:id that returns the talker with specified id

--- routes/talkerRoutes.js
+++ routes/talkerRoutes.js
@@ -10,4 +10,13 @@ router.get('/', async (req, res) => {
   return res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkers();
+  const talker = talkers.find((t) => t.id === parseInt(id, 10));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talker);
+});
+
 module.exports = router;
