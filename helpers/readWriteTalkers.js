5c0b782d737f34fd074366b27c595299a29f4148
Guilherme <guilhaugusto05@gmail.com>
2022-03-18 02:11:28 -0300

    Create writeTalkers function

--- helpers/readWriteTalkers.js
+++ helpers/readWriteTalkers.js
@@ -3,11 +3,25 @@ const fs = require('fs').promises;
 const readTalkers = async () => {
   try {
     const talkers = await fs.readFile('./talker.json', 'utf8');
-    return JSON.parse(talkers);
+    return talkers ? JSON.parse(talkers) : [];
   } catch (err) {
     console.log(err);
     return null;
   }
 };
 
-module.exports = { readTalkers };
+const writeTalkers = async (talker) => {
+  try {
+    const talkers = await readTalkers();
+
+    talkers.push(talker);
+    await fs.writeFile('./talker.json', JSON.stringify(talkers, null, 2));
+
+    return talker;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+module.exports = { readTalkers, writeTalkers };
